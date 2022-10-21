a8c3f4fc0a8eea7f70c0d892084a2bd4a8765a18
AlexDiv22 <107955694+AlexDiv22@users.noreply.github.com>
2022-10-21 09:30:37 +0400

    dz1

---
+++ dz1_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class dz1_1 {
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите числовое значение для треугольного числа: ");
+        int n = iScanner.nextInt();
+        System.out.println((n*(n+1))/2);
+        iScanner.close();
+
+
+        show (n,0);
+
+    }    
+
+    static void show (int n, int count) {
+        
+        for (int i = 0; i < count; i++) {
+            System.out.printf("  ");      
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.printf("  X ");       
+        }
+        System.out.println();
+        if (n==1) return;
+        show(n-1, ++count);
+    }
+
+}
